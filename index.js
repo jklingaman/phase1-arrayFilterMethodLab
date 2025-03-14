03f00aa9223e4d72ac80440e349c1eae84851afa
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-03-14 14:23:06 -0500

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driverString) {
+      return driverString.toLowerCase() === name.toLowerCase();
+    })
+  }
+
+function fuzzyMatch(drivers, nameAbv) {
+  return drivers.filter(function(driverName) { 
+      return driverName.slice(0, 2) === nameAbv
+  })
+}
+
+function matchName(drivers, input) {
+  return drivers.filter(function(nameMatch) {
+    if (input === nameMatch.name) {
+      return nameMatch;
+    }
+  })
+}
+
+
+// command foraward slash
+  
